43fdbe157acab4ede991a095d997c966f9eed7b9
Jhon Trujillo Serrano <jhontruse@gmail.com>
2025-03-23 23:40:40 -0500

    Se agrego detalles en el modulo de modulos

--- src/app/pages/modulo/modulo.component.ts
+++ src/app/pages/modulo/modulo.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Button} from 'primeng/button';
+import {Divider} from 'primeng/divider';
+import {FloatLabel} from 'primeng/floatlabel';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {InputTextModule} from 'primeng/inputtext';
+import {CalendarModule} from 'primeng/calendar';
+import {FloatLabelModule} from 'primeng/floatlabel';
+import {ToggleSwitch} from 'primeng/toggleswitch';
+import {NgClass} from '@angular/common';
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-modulo',
-  imports: [],
+  imports: [
+    Button,
+    Divider,
+    FloatLabel,
+    FormsModule,
+    InputTextModule,
+    CalendarModule,
+    FloatLabelModule,
+    ReactiveFormsModule,
+    ToggleSwitch,
+    NgClass,
+  ],
   templateUrl: './modulo.component.html',
   styleUrl: './modulo.component.css'
 })
-export class ModuloComponent {
+export class ModuloComponent implements OnInit {
+  form: FormGroup;
+
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      moduloId: new FormControl(''),
+      moduloNombre: new FormControl(''),
+      moduloUrl: new FormControl(''),
+      moduloIcono: new FormControl(''),
+      moduloFecRegistro: new FormControl(''),
+      moduloFecActu: new FormControl(''),
+      moduloMcaInh: new FormControl(''),
+      moduloInputArea: new FormControl('')
+    });
+  }
+
+  operate() {
+
+  }
 
 }
